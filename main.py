e0be9dfb47c78f41623d8ed35ceb879ce8091dc7
Yousha Kabir <youshasafwan@gmail.com>
2026-05-09 01:29:45 +1000

    Add game finished loop and catch for ctrl v + d

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from collections import deque
 
 KNOT = "O"
@@ -7,20 +8,30 @@ PLAYER2 = -1
 UNSET = ' '
 
 playerToMove = {PLAYER1: KNOT, PLAYER2: CROSS}
+moveToPlayer = {KNOT: PLAYER1, CROSS: PLAYER2}
 
 # hot seat feature, show which cells are expiring
 # choose what char for each player
-# solve board feat (when game ends)
+# quit game button
+# play AI mode
 
 def get_player_action(board: list[list[chr]]) -> tuple:
     while True:
         try:
-            choice = tuple(map(int, input("Enter row col: ").split()))
+            choice = tuple(map(int, input("\nEnter row col: ").split()))
             
         except ValueError:
             print("Please enter valid input!")
             continue
 
+        except KeyboardInterrupt:
+            print("\nSad to see you go :(")
+            sys.exit(0)
+            
+        except EOFError:
+            print("\nSad to see you go :(")
+            sys.exit(0)
+            
         if len(choice) != 2:
             print("Please enter as row col")
             continue
@@ -31,7 +42,7 @@ def get_player_action(board: list[list[chr]]) -> tuple:
             print("Out of bounds!")
             continue
         
-        if board[row][col] != UNSET:
+        if board[row][col] == KNOT or board[row][col] == CROSS:
             print("Choice is occupied!")
             continue
         
@@ -47,8 +58,40 @@ def update_board(move, board, currPlayer):
     board[move[0]][move[1]] = playerToMove[currPlayer]
     return board
 
-def game_finished():
-    pass
+def checkVertical(board: list[list[chr]], playerSymbol: str):
+    if board[0][0] == playerSymbol and board[1][0] == playerSymbol and board[2][0] == playerSymbol:
+        return True
+    if board[0][2] == playerSymbol and board[1][2] == playerSymbol and board[2][2] == playerSymbol:
+        return True
+    return None
+
+def checkDiagonal(board: list[list[chr]], playerSymbol: str):
+    if board[0][0] == playerSymbol and board[1][1] == playerSymbol and board[2][2] == playerSymbol:
+        return True
+    if board[0][2] == playerSymbol and board[1][1] == playerSymbol and board[2][0] == playerSymbol:
+        return True
+    return None
+    
+def game_finished(board: list[list[chr]]):
+    # check horizontal
+    for row in board:
+        if len(set(row)) == 1 and UNSET not in row:
+            # row is all x or o
+            return moveToPlayer[row[0]]
+    
+    if checkVertical(board, playerToMove[PLAYER1]) is not None:
+        return PLAYER1
+    elif checkVertical(board, playerToMove[PLAYER2]) is not None:
+        return PLAYER2
+    
+    # check diagonal
+    if checkDiagonal(board, playerToMove[PLAYER1]) is not None:
+        return PLAYER1
+    elif checkDiagonal(board, playerToMove[PLAYER2]) is not None:
+        return PLAYER2
+    
+    return None
+    
 
 def main():
     board = [
@@ -60,7 +103,7 @@ def main():
     queue = deque()
     
     print_board(board)
-    while (1):
+    while (game_finished(board) is None):
         move = get_player_action(board)
         queue.append(move)
         if len(queue) == 6:
@@ -68,11 +111,17 @@ def main():
             board[expiredMove[0]][expiredMove[1]] = UNSET
         update_board(move, board, currPlayer)
         print_board(board)
+        # switch player
         currPlayer = -currPlayer
-    # while not game_finished():
-    #     print_board(board)
-    #     get_player_action()
-    #     update_board()
+    
+    winner = game_finished(board)
+    if winner == PLAYER1:
+        winner = "Player 1"
+    else:
+        winner = "Player 2"
+    
+    print(f"\n*** {winner} is the winner! ***")
+
 
 if __name__ == "__main__":
     main()
